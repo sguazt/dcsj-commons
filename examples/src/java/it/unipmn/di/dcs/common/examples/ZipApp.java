00b71b04d953cc9e32f03b82b958ed2f43082bca
sguazt <marco.guazzone@gmail.com>
2012-11-15 22:18:20 +0100

    (change:minor) Updated licensing stuff.

--- examples/src/java/it/unipmn/di/dcs/common/examples/ZipApp.java
+++ examples/src/java/it/unipmn/di/dcs/common/examples/ZipApp.java
@@ -1,19 +1,25 @@
 /*
- * Copyright (C) 2008  Distributed Computing System (DCS) Group, Computer
- * Science Department - University of Piemonte Orientale, Alessandria (Italy).
+ * Copyright (C) 2008-2012  Marco Guazzone
+ *                          [Distributed Computing System (DCS) Group,
+ *                           Computer Science Institute,
+ *                           Department of Science and Technological Innovation,
+ *                           University of Piemonte Orientale,
+ *                           Alessandria (Italy)]
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of dcj-commons.
+ *
+ * dcsj-commons is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * dcsj-commons is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with dcsj-commons.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package it.unipmn.di.dcs.common.examples;
@@ -26,7 +32,7 @@ import java.util.List;
 /**
  * A simple zip application.
  *
- * @author <a href="mailto:marco.guazzone@mfn.unipmn.it">Marco Guazzone</a>
+ * @author <a href="mailto:marco.guazzone@gmail.com">Marco Guazzone</a>
  */
 public class ZipApp
 {
